d7542452530db9772a7cd548a48c309658fb21ce
girlswhocode-sjsu <49962214+saib2418@users.noreply.github.com>
2023-01-04 22:39:54 -0800

    loading button

--- script.js
+++ script.js
@@ -3,11 +3,22 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 //Show new quote
 function newQuote() {
+  loading();
   //Pick a random quote for API quotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   //Check if author field is blank --> replace with 'unkown'
@@ -23,10 +34,12 @@ function newQuote() {
     quoteText.classList.remove("long-quote");
   }
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get Quotes From API
 async function getQuotes() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
@@ -48,7 +61,7 @@ twitterBtn.addEventListener("click", tweetQuote);
 
 //On load
 getQuotes();
-// loading();
+loading();
 
 //if we used the "quotes.js" file, we can delete all the code in this file (since we aren't
 //fetching naything from an API anymore, its just a bunch of static data)
